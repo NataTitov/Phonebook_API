048f93276422efe0820771079b1fc7effbc43017
pataha <pataha@gmx.de>
2025-06-02 17:48:01 +0200

    add updateContact negativeTests + AfterMethod

--- src/test/java/com/phonebook/tests/restassured/UpdateContactRATests.java
+++ src/test/java/com/phonebook/tests/restassured/UpdateContactRATests.java
@@ -3,6 +3,7 @@ package com.phonebook.tests.restassured;
 import com.phonebook.dto.ContactDto;
 import com.phonebook.dto.UpdateContactDto;
 import io.restassured.http.ContentType;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -36,7 +37,7 @@ public class UpdateContactRATests extends TestBase {
 
     }
     @Test
-    public void updateContactTest() {
+    public void updateContactPositiveTest() {
         UpdateContactDto updateContact = UpdateContactDto.builder()
                 .id(id)
                 .name("Thomas")
@@ -57,5 +58,59 @@ public class UpdateContactRATests extends TestBase {
                 .assertThat().body("message", equalTo("Contact was updated"));
 
 
+    }
+    @Test
+    public void updateContactWithWrongPhoneNegativeTest() {
+        UpdateContactDto updateContact = UpdateContactDto.builder()
+                .id(id)
+                .name("Thomas")
+                .lastName("Smith")
+                .email("TS@gm.com")
+                .phone("123")
+                .address("Berlin")
+                .description("Description")
+                .build();
+
+                given().header(AUTHORIZATION, TOKEN)
+                .body(updateContact)
+                .contentType(ContentType.JSON)
+                .when()
+                .put("contacts")
+                .then()
+                .assertThat().statusCode(400)
+                .assertThat().body("message.phone", equalTo("Phone number must contain only digits! And length min 10, max 15!"));
+
+    }
+    @Test
+    public void updateContactWithWrongIDNegativeTest() {
+        UpdateContactDto updateContact = UpdateContactDto.builder()
+                .id("7f3eb125-53e3-4af2-b6e1-fbdf742c47e0")
+                .name("Thomas")
+                .lastName("Smith")
+                .email("TS@gm.com")
+                .phone("1234567890")
+                .address("Berlin")
+                .description("Description")
+                .build();
+
+        given().header(AUTHORIZATION, TOKEN)
+                .body(updateContact)
+                .contentType(ContentType.JSON)
+                .when()
+                .put("contacts")
+                .then()
+                .assertThat().statusCode(403)
+                .assertThat().body("message", equalTo("Contact with id: 7f3eb125-53e3-4af2-b6e1-fbdf742c47e0 not found in your contacts!"));
+    }
+    @AfterMethod
+    public void deleteContact() {
+        given()
+                .header(AUTHORIZATION, TOKEN)
+                .when()
+                .delete("contacts/" + id)
+                .then()
+                .assertThat().statusCode(200)
+                .assertThat().body("message", equalTo("Contact was deleted!"))
+        ;
     }
 }
